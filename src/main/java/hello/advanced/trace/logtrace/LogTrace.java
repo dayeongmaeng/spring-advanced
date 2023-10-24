0dc34a7d97d0558b4c70664722fa92ad5826322c
Dayeong <dayeongmaeng@gmail.com>
2023-10-24 23:26:56 +0900

    feat: 필드 동기화 - 적용

--- src/main/java/hello/advanced/trace/logtrace/LogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -1,11 +1,11 @@
-package hello.advanced.logtrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceStatus;
 
 public interface LogTrace {
 
     TraceStatus begin(String message);
- 
+
     void end(TraceStatus status);
 
     void exception(TraceStatus status, Exception e);
